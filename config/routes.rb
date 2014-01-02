9856d8dd38f271f94dff163c6c298d56915a195c
Helen Hood <helen.c.hood@gmail.com>
2014-01-02 08:27:58 -0500

    Added Devise (followed all config instructions)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TimeCatcherCapybara::Application.routes.draw do
+
+  root :to => "home#index"  #added per devise install instructions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
