4b17656c88fcc5d1f84183044ffdd955242395ae
Helen Hood <helen.c.hood@gmail.com>
2014-01-02 08:53:22 -0500

    Added attributes to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
+    first_name "Jane"
+    last_name "Smith"
+    sequence(:email) { |n| "jsmith#{n}@example.com"}
+    password "secret_password"
+    password_confirmation "secret_password"
   end
 end
