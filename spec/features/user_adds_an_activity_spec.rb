ea662fd5f5e77aef720d8fd6a587bba84b6bec42
Helen Hood <helen.c.hood@gmail.com>
2014-01-03 11:33:53 -0500

    Add acceptance test for add_activity feature

---
+++ spec/features/user_adds_an_activity_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature 'Authenticated user adds an activity', %Q{
+  As an authenticated user
+  I want to add an activity
+  So that the app can tell me to complete the activity when I use it
+} do
+
+  # Acceptance Criteria:
+  #  * I must be signed in to add an activity
+  #  * If I’m not signed in, I am not allowed access to add an activity
+  #  * I must specify an activity name, estimated minimum time to complete the activity.
+  #  * I can optionally specify a description
+  #  * If I specify the required details, the activity is logged & I receive a friendly confirmation!
+  #  * If I don’t, I’m presented with error messages
+  #  * If the name of the activity I entered is already in my activity list, I receive an error message.
+
+  scenario 'adds an activity with valid attributes' do
+    user = FactoryGirl.create(:user)
+    login(user)
+
+    activity = FactoryGirl.create(:activity)
+    click_on 'Add Activity'
+    fill_in 'Name', with: activity.name
+    fill_in 'Description', with: activity.description
+    fill_in 'Time Needed', with: activity.time_needed_in_min
+    click_button 'Create Activity'
+
+    expect(page).to have_content 'Activity was successfully created.'
+  end
+
+  scenario 'adds an activity without required attributes'
+
+  scenario 'adds an activity already in the list'
+
+  scenario 'unauthenticated user attempts to add an activity'
+end
