78587cbaef6070789752173340e6ae851a14c6f4
Helen Hood <helen.c.hood@gmail.com>
2014-01-08 19:48:57 -0500

    Add validations for user_id, destroy dependency

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
+  validates_presence_of :user_id
+
   validates_uniqueness_of :name
 
   has_many :activities, dependent: :nullify
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Category do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :user_id }
   it { should validate_uniqueness_of :name }
 
   it { should have_many(:activities).dependent(:nullify) }
-  it { should belong_to :user }
+  it { should belong_to(:user).dependent(:destroy) }
 end
