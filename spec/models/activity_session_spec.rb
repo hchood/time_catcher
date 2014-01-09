ac3aeb144e5fb8bc0a39c68f7b891ca2cc1b58b4
Helen Hood <helen.c.hood@gmail.com>
2014-01-09 12:25:16 -0500

    Fix simple_form errors

--- spec/models/activity_session_spec.rb
+++ spec/models/activity_session_spec.rb
@@ -4,6 +4,8 @@ describe ActivitySession do
   it { should validate_presence_of :activity_id }
   it { should validate_presence_of :time_available }
   it { should validate_numericality_of(:time_available).is_greater_than(0) }
+  it { should have_valid(:time_available).when(1, 5) }
+  it { should_not have_valid(:time_available).when('abd', 0) }
 
   it { should belong_to(:activity).dependent(:destroy) }
 
