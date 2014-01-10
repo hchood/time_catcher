42725d4e2e00a217a0c63d6c0175f4f8b75040b8
Helen Hood <helen.c.hood@gmail.com>
2014-01-10 17:45:59 -0500

    Root to new_activity_session_path if user signed_in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-
+    redirect_to new_activity_session_path if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TimeCatcherCapybara::Application.routes.draw do
 
   devise_for :users
-  root :to => "home#index"  #added per devise install instructions
+  root to: "home#index"  #added per devise install instructions
 
   resources :activities, only: [:index, :new, :create]
   resources :categories, only: [:index, :new, :create]
