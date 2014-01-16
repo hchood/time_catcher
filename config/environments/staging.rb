6422a84f2788685a23cc647f4ee0353ca6bae6e9
Helen Hood <helen.c.hood@gmail.com>
2014-01-16 14:19:04 -0500

    Config action_mailer default options in production & staging env files

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,4 +77,5 @@ TimeCatcherCapybara::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'timecatcher-staging.herokuapp.com' }
 end
