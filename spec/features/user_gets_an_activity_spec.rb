2a4e71363f7dee570c7688832a49c8777cedf23c
Helen Hood <helen.c.hood@gmail.com>
2014-01-22 19:14:54 -0500

    Refactor user_gets_an_activity_spec

--- spec/features/user_gets_an_activity_spec.rb
+++ spec/features/user_gets_an_activity_spec.rb
@@ -17,103 +17,102 @@ feature 'Authenticated user gets an activity', %Q{
   #    * I am presented with an activity from my activity list that has a “minimum estimated time” that is less than or equal to the amount of time I entered.
   #   * If there are no activities in my list under that time limit, I receive an error message and a link to "Add Activities" is displayed.
 
-  let!(:user) { FactoryGirl.create(:user) }
+  context 'authenticated user' do
+    let!(:user) { FactoryGirl.create(:user) }
 
-  scenario 'user provides valid time available and is given an activity' do
-    short_activity = FactoryGirl.create(:activity, user: user)
-    med_activity = FactoryGirl.create(:activity, user: user, time_needed_in_min: 10, description: 'A different activity')
-    long_activity = FactoryGirl.create(:activity, user: user, time_needed_in_min: 15, description: 'Activity is too long')
+    context 'who has activities' do
+      before :each do
+        @short_activity = FactoryGirl.create(:activity, user: user)
+        @med_activity = FactoryGirl.create(:activity, user: user, time_needed_in_min: 10, description: 'A different activity')
+        @long_activity = FactoryGirl.create(:activity, user: user, time_needed_in_min: 15, description: 'Activity is too long')
+        login(user)
+      end
 
-    login(user)
-    fill_in 'activity_session[time_available]', with: 10
-    click_on 'Give me something to do!'
+      scenario 'provides valid time available and is given an activity' do
+        fill_in 'activity_session[time_available]', with: 10
+        click_on 'Give me something to do!'
 
-    expect_page_to_have_one_of([short_activity.name, med_activity.name])
-    expect_page_to_have_one_of([short_activity.description, med_activity.description])
+        expect_page_to_have_one_of([@short_activity.name, @med_activity.name])
+        expect_page_to_have_one_of([@short_activity.description, @med_activity.description])
 
-    expect(page).to_not have_content long_activity.name
-    expect(page).to_not have_content long_activity.description
+        expect(page).to_not have_content @long_activity.name
+        expect(page).to_not have_content @long_activity.description
 
-    # correct buttons are displayed
-    expect(page).to have_button "I'm done!"
-    expect(page).to have_button 'Skip'
+        # correct buttons are displayed
+        expect(page).to have_button "I'm done!"
+        expect(page).to have_button 'Skip'
 
-    # ActivitySession object is created
-    expect(ActivitySession.count).to eq 1
-    expect(ActivitySession.first.activity).to_not be_nil
-  end
+        # ActivitySession object is created
+        expect(ActivitySession.count).to eq 1
+        expect(ActivitySession.first.activity).to_not be_nil
+      end
 
-  scenario 'unauthenticated user cannot get an activity' do
-    visit '/activity_sessions/new'
+      context 'provides invalid time available' do
+        scenario 'provides a string' do
+          fill_in 'activity_session[time_available]', with: 'abc'
+          click_on 'Give me something to do!'
 
-    expect(page).to have_content 'You need to sign in or sign up before continuing'
-    expect(page).to_not have_button "Let's go!"
-  end
+          expect(page).to have_content 'You must enter a number greater than 0.'
+          expect(page).to have_button 'Give me something to do!'
 
-  scenario 'user has no activities' do
-    login(user)
+          expect(ActivitySession.count).to eq 0
+        end
 
-    expect(page).to have_content "You haven't added any activities yet."
-    expect(page).to have_link 'here'
-  end
+        scenario 'provides a negative number' do
+          fill_in 'activity_session[time_available]', with: -5
+          click_on 'Give me something to do!'
 
-  scenario 'user has no activities that can be done in the time available' do
-    short_activity = FactoryGirl.create(:activity, user: user)
-    med_activity = FactoryGirl.create(:activity, user: user, time_needed_in_min: 10)
+          expect(page).to have_content 'You must enter a number greater than 0.'
+          expect(page).to have_button 'Give me something to do!'
 
-    login(user)
-    fill_in 'activity_session[time_available]', with: 3
-    click_button 'Give me something to do!'
+          expect(ActivitySession.count).to eq 0
+        end
 
-    expect(page).to have_content "Sorry, you don't have any activities you can do in 3 minutes."
-    expect(page).to have_link 'Add Activity'
-  end
+        scenario 'provides 0' do
+          fill_in 'activity_session[time_available]', with: 0
+          click_on 'Give me something to do!'
 
-  context 'user enters an invalid time or leaves it blank' do
-    let!(:activity) { FactoryGirl.create(:activity, user: user) }
+          expect(page).to have_content 'You must enter a number greater than 0.'
+          expect(page).to have_button 'Give me something to do!'
 
-    scenario 'displays an error if a string is input' do
-      login(user)
-      fill_in 'activity_session[time_available]', with: 'abc'
-      click_on 'Give me something to do!'
+          expect(ActivitySession.count).to eq 0
+        end
 
-      expect(page).to have_content 'You must enter a number greater than 0.'
-      expect(page).to have_button 'Give me something to do!'
+        scenario 'leaves time available blank' do
+          click_on 'Give me something to do!'
 
-      expect(ActivitySession.count).to eq 0
+          expect(page).to have_content 'You must enter a number greater than 0.'
+          expect(page).to have_button 'Give me something to do!'
+
+          expect(ActivitySession.count).to eq 0
+        end
+      end
+
+      scenario 'has no activities that can be done in the time available' do
+        fill_in 'activity_session[time_available]', with: 3
+        click_button 'Give me something to do!'
+
+        expect(page).to have_content "Sorry, you don't have any activities you can do in 3 minutes."
+        expect(page).to have_link 'Add Activity'
+      end
     end
 
-    scenario 'displays an error if inputs negative number' do
-      login(user)
-      fill_in 'activity_session[time_available]', with: -5
-      click_on 'Give me something to do!'
+    context 'who does not have activities' do
+      scenario 'is not given an activity to do' do
+        login(user)
 
-      expect(page).to have_content 'You must enter a number greater than 0.'
-      expect(page).to have_button 'Give me something to do!'
-
-      expect(ActivitySession.count).to eq 0
+        expect(page).to have_content "You haven't added any activities yet."
+        expect(page).to have_link 'here'
+      end
     end
+  end
 
-    scenario 'displays an error if inputs 0' do
-      login(user)
-      fill_in 'activity_session[time_available]', with: 0
-      click_on 'Give me something to do!'
+  context 'unauthenticated user' do
+    scenario 'cannot get an activity' do
+      visit '/activity_sessions/new'
 
-      expect(page).to have_content 'You must enter a number greater than 0.'
-      expect(page).to have_button 'Give me something to do!'
-
-      expect(ActivitySession.count).to eq 0
-    end
-
-
-    scenario 'displays an error if leaves time blank' do
-      login(user)
-      click_on 'Give me something to do!'
-
-      expect(page).to have_content 'You must enter a number greater than 0.'
-      expect(page).to have_button 'Give me something to do!'
-
-      expect(ActivitySession.count).to eq 0
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+      expect(page).to_not have_button "Let's go!"
     end
   end
 
